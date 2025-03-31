fabab52545ce3e1f5b0061f6c1b378858f28a330
Junior da Silveira Gomes <junior.silgo@gmail.com>
2025-03-30 23:40:57 -0300

    Inclusão do métod delete na rota de alunos e inclusão dos métodos post, get, put, patch, delete na rota de cursos - atualização do readme

--- README.md
+++ README.md
@@ -17,7 +17,28 @@ Rota para cadastrar um(a) aluno(a):
 Rota para atualizar totalmente um(a) aluno(a) a partir de um ID:
 - /alunos/atualizarTotal/:id
 
-Rota para atualizar parcialmente um aluno um(a) aluno(a) a partir de um ID:
+Rota para atualizar parcialmente um(a) aluno(a) a partir de um ID:
 - /alunos/atualizarParcial/:id
 
+Rota para deletar um(a) aluno(a):
+- /alunos/excluir/:id
+
 - CURSOS:
+
+Rota para exibir todos cursos cadastrados:
+- /cursos/listar
+
+Rota para exibir um curso a partir de um ID:
+- /cursos/visualizar/:id
+
+Rota para cadastrar um curso:
+- /cursos/cadastrar
+
+Rota para atualizar totalmente um curso a partir de um ID:
+- /cursos/atualizarTotal/:id
+
+Rota para atualizar parcialmente um curso a partir de um ID:
+- /cursos/atualizarParcial/:id
+
+Rota para deletar um curso:
+- /cursos/excluir/:id
--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.post("/alunos/cadastrar", (req, res) => {
     let id = alunos.length ? alunos.at(-1).id + 1 : 1;
 
     if (!nome || !matricula) {
-        res.status(404).json(`Os campos nome e matricula precisam ser informados!`);
+        res.status(404).json(`Os campos Nome e Matrícula precisam ser informados!`);
     }
 
     let aluno = {
@@ -41,18 +41,18 @@ app.post("/alunos/cadastrar", (req, res) => {
         matricula
     };
     alunos.push(aluno);
-    res.status(201).json(`ID: ${id} - Aluna(o): ${nome} - Matricula ${matricula}. Cadastrada(o) com sucesso!`);
+    res.status(201).json(`ID: ${id} - Aluna(o): ${nome} - Matrícula: ${matricula}. Cadastrada(o) com sucesso!`);
 });
 
 app.put("/alunos/atualizarTotal/:id", (req, res) => {
-    let { id } = req.params;
+    let id = parseInt(req.params.id);
     let { nome, matricula } = req.body;
 
     if (!nome || !matricula) {
-        res.status(404).json(`Os campos nome e matricula precisam ser informados!`);
+        res.status(404).json(`Os campos Nome e Matrícula precisam ser informados!`);
     }
     
-    let busca = alunos.find((aluno) => aluno.id === parseInt(req.params.id));
+    let busca = alunos.find((aluno) => aluno.id === id);
 
     if(busca) {
         alunos[id - 1] = {
@@ -60,37 +60,151 @@ app.put("/alunos/atualizarTotal/:id", (req, res) => {
             nome,
             matricula
         };
-        res.status(200).json(`ID: ${id} - Aluna(o): ${nome} - Matricula: ${matricula}. Atualizada(o) com sucesso!`);
+        res.status(200).json(`ID: ${id} - Aluna(o): ${nome} - Matrícula: ${matricula}. Atualizada(o) com sucesso!`);
     } else {
         res.status(404).json("Aluna(o) não encontrada(o)!");
     };
 });
 
 app.patch("/alunos/atualizarParcial/:id", (req, res) => {
-    let { id } = req.params;
+    let id = parseInt(req.params.id);
     let { nome, matricula } = req.body;
 
     if (!nome && !matricula) {
-        res.status(404).json('Pelo menos um dos campos(Nome ou Matricula) precisa ser informado!');
+        res.status(404).json('Pelo menos um dos campos(Nome ou Matrícula) precisa ser informado!');
     }
     
-    let busca = alunos.find((aluno) => aluno.id === parseInt(req.params.id));
+    let busca = alunos.find((aluno) => aluno.id === id);
 
     if(busca) {
         alunos[id - 1] = {
             ...busca,
+            id: parseInt(id),
             ...req.body
         };       
-        res.status(200).json(`ID: ${alunos[id - 1].id} - Aluna(o): ${alunos[id - 1].nome} - Matricula: ${alunos[id - 1].matricula}. Atualizada(o) com sucesso!`);
+        res.status(200).json(`ID: ${alunos[id - 1].id} - Aluna(o): ${alunos[id - 1].nome} - Matrícula: ${alunos[id - 1].matricula}. Atualizada(o) com sucesso!`);
     } else {
         res.status(404).json("Aluna(o) não encontrado!");
     };
 
 });
 
+app.delete("/alunos/excluir/:id", (req, res) => {
+    let id = parseInt(req.params.id);
+
+    let verificacao = alunos.find((aluno) => aluno.id === id);
+    let nome = verificacao ? verificacao.nome : '';
+
+    if(verificacao){
+        let busca = alunos.filter((aluno) => aluno.id !== id);
+        alunos = busca;        
+        res.status(200).json(`O aluno: ${nome} foi excluído com sucesso!`);
+    } else {
+        res.status(404).json(`A(O) Aluna(o) com o ID: ${id} não foi encontrada(o)!`);
+    };
+});
+
 // ROTAS CURSOS:
+
+let cursos = [];
+
+app.get("/cursos/listar", (_req, res) => {
+    if(cursos.length > 0) {
+        res.status(200).json(cursos);
+    } else {
+        res.status(404).json("Não há cursos cadastrados!");
+    };
+})
+
+app.get("/cursos/visualizar/:id", (req, res) => {
+    let busca = cursos.find((curso) => curso.id === parseInt(req.params.id));
+
+    if(busca) {
+        res.status(200).json(busca);
+    } else {
+        res.status(404).json("Curso não encontrado!");
+    };
+})
+
+app.post("/cursos/cadastrar", (req, res) => {
+    let { nome, codigo } = req.body;
+    let id = cursos.length ? cursos.at(-1).id + 1 : 1;
+
+    if (!nome || !codigo) {
+        res.status(404).json(`Os campos Nome e Código precisam ser informados!`);
+    }
+
+    let curso = {
+        id,
+        nome,
+        codigo
+    };
+    cursos.push(curso);
+    res.status(201).json(`ID: ${id} - Curso: ${nome} - Código: ${codigo}. Cadastrada(o) com sucesso!`);
+});
+
+app.put("/cursos/atualizarTotal/:id", (req, res) => {
+    let id = parseInt(req.params.id);
+    let { nome, codigo } = req.body;
+
+    if (!nome || !codigo) {
+        res.status(404).json(`Os campos Nome e Código precisam ser informados!`);
+    }
+    
+    let busca = cursos.find((curso) => curso.id === id);
+
+    if(busca) {
+        cursos[id - 1] = {
+            id: parseInt(id),
+            nome,
+            codigo
+        };
+        res.status(200).json(`ID: ${id} - Curso: ${nome} - Código: ${codigo}. Atualizada(o) com sucesso!`);
+    } else {
+        res.status(404).json("Curso não encontrada(o)!");
+    };
+});
+
+app.patch("/cursos/atualizarParcial/:id", (req, res) => {
+    let id = parseInt(req.params.id);
+    let { nome, codigo } = req.body;
+
+    if (!nome && !codigo) {
+        res.status(404).json('Pelo menos um dos campos(Nome ou Código) precisa ser informado!');
+    }
+    
+    let busca = cursos.find((curso) => curso.id === id);
+
+    if(busca) {
+        cursos[id - 1] = {
+            ...busca,
+            id: parseInt(id),
+            ...req.body
+        };       
+        res.status(200).json(`ID: ${cursos[id - 1].id} - Curso: ${cursos[id - 1].nome} - Código: ${cursos[id - 1].codigo}. Atualizada(o) com sucesso!`);
+    } else {
+        res.status(404).json("Curso não encontrado!");
+    };
+
+});
+
+app.delete("/cursos/excluir/:id", (req, res) => {
+    let id = parseInt(req.params.id);
+
+    let verificacao = cursos.find((curso) => curso.id === id);
+    let nome = verificacao ? verificacao.nome : '';
+
+    if(verificacao){
+        let busca = cursos.filter((curso) => curso.id !== id);
+        cursos = busca;        
+        res.status(200).json(`O curso: ${nome} foi excluído com sucesso!`);
+    } else {
+        res.status(404).json(`O curso com o ID: ${id} não foi encontrada(o)!`);
+    };
+});
+
+// 
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
